9ca6cfcda1ef8698fcf5230abca51789e2ced7f6
Joel <joeldonaldakers@gmail.com>
2019-01-19 11:04:17 -0500

    Test

--- models/Future.js
+++ models/Future.js
@@ -1,10 +1,4 @@
-// Dependencies
-// =============================================================
 
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
 
 module.exports = function(sequelize, DataTypes) {
   var Future = sequelize.define("past", {
