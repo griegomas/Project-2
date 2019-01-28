21c8573a4c08999c9b8cd7257376f65032267534
Evan Kozierachi <evan.kozierachi@gmail.com>
2019-01-27 18:40:38 -0800

    final2

--- server.js
+++ server.js
@@ -12,15 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
